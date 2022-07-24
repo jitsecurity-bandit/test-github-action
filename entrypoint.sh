8da0274828bc317bbe0aae1b04c4620659eea95d
liormw <lior@jit.io>
2022-07-24 22:02:46 +0300

    Added wrapping braces.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 export URL=http://046b-85-65-234-136.ngrok.io/send_some_info
 
-curl -X POST --retry $1 --retry-delay $2 -H "Content-Type: application/json" --data $3 --fail $URL
+curl -X POST --retry $1 --retry-delay $2 -H "Content-Type: application/json" --data "$3" --fail $URL
